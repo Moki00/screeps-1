f3ddc56666f926d9a8dc2f8de3de05327b0ea1d4
kamsac <kamsac@users.noreply.github.com>
2018-07-16 22:17:51 +0200

    Add jest for unit tests. Make Screeps global contants work in tests.

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'json',
+    'node'
+  ],
+  globals: require('@screeps/common/lib/constants')
+};
