73ca812b1c26aab7d594a9346c88a94ff5bafa0b
kamsac <kamsac@users.noreply.github.com>
2020-11-24 14:00:40 +0100

    Disable generating pixels on servers where they don't exist.

--- src/utils/update-generate-pixels.ts
+++ src/utils/update-generate-pixels.ts
@@ -4,6 +4,7 @@ const SCREEPS_CPU_BUCKET_MAX = 10000;
 
 export default function updateGeneratePixels(): void {
   const shouldGeneratePixel =
+    Game.cpu.generatePixel &&
     Game.cpu.bucket > SCREEPS_CPU_BUCKET_MAX - Game.cpu.limit;
   if (shouldGeneratePixel) {
     switch (Game.cpu.generatePixel()) {
